0836e784a3448def3beffbb776506cc41ef5a50b
Van Nguyen _ Bachelor of Computer Science <152344203+Minhvan1705@users.noreply.github.com>
2025-10-16 03:40:51 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+
+/**********************************************************
+ * Create a BlueJ project, call is Hello and print out 
+ * screen the string "Hello world"
+ * 
+ * @author (Van Nguyen)
+ * @version (Sunday January 23th, 2022)
+ **********************************************************/
+
+// A program in java that prints "Hello world" 
+
+   public class Hello         // class header Hello
+   {
+         public static void main (String[] args)    //  method header
+         {
+             System.out.print( " Hello world! " );   /* output to screen a
+                                                        string Hello world */
+         }
+    }           // the end.
