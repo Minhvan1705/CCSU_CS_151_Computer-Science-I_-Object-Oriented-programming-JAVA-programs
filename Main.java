d3a053fc2296b3bf943803cb2e0fe69e351dc4d1
Van Nguyen _ Bachelor of Computer Science <152344203+Minhvan1705@users.noreply.github.com>
2025-10-16 04:49:15 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/** CS 151 PROGRAM ASSIGNMENT # 5
+ * 
+ * 
+ * BMI  ( the Body Mass Index )
+ * 
+ * @author (Van Nguyen)
+ * @version (February 28th, 2022)
+ ********************************************************************/
+
+ 
+ public class Main {
+  public static void main(String[] args) {
+    String str = "What passing-bells for these who die as cattle?" ;    
+    System.out.println( str.substring(5,12).toUpperCase() );
+  }
+}
+
+ 
