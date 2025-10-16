fd061ff036c1cfe1846f9ad2584cbad85ab6b969
Van Nguyen _ Bachelor of Computer Science <152344203+Minhvan1705@users.noreply.github.com>
2025-10-16 03:48:20 -0400

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,24 @@
+
+/**********************************************************
+ * A program in Java that does arithmetic
+ *
+ * @author (Van Nguyen)
+ * @version (Sunday January 23th, 2022)
+ **********************************************************/
+ 
+public class Arithmetic         // header of class Arithmetic
+{
+    public static void main(String[]args)
+    { 
+         double a,b, result;                 // declare variable
+         a = 4;                   
+         b = 2;                        // value of variaable    
+         
+         result = a+b;          // assign expression
+         System.out.print( " The result 1 : "+ result); //output result1's value
+         
+         result = 2.0/4;
+         System.out.print( " The result 2 : "+ result);// print screen a value
+         System.out.println();
+    }
+}             // close class
